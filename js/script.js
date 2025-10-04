74ead78ae71e422a17b5b20eb3400928e8d5e8b9
Ліза Гоцко <filani9818@gmail.com>
2025-10-04 23:50:18 +0300

    added more js

--- js/script.js
+++ js/script.js
@@ -1,15 +1,26 @@
 document.addEventListener('DOMContentLoaded', function () {
   const modal = document.querySelector('.modal-mob-overlay');
+  const service_modal = document.querySelector('.modal-overlay');
   const button = document.querySelector('button');
+  const service_button = document.querySelector('.hero-button');
   const close = document.querySelector('.close');
+  const service_close = document.querySelector('.modal-close-btn');
   const content = document.querySelector('.content');
 
   button.addEventListener('click', () => {
     modal.classList.add('is-open');
   });
 
+  service_button.addEventListener('click', () => {
+    service_modal.classList.add('is-open');
+  });
+
   close.addEventListener('click', () => {
     modal.classList.remove('is-open');
+  });
+
+  service_close.addEventListener('click', () => {
+    service_modal.classList.remove('is-open');
   });
 
   modal.addEventListener('click', e => {
@@ -21,6 +32,7 @@ document.addEventListener('DOMContentLoaded', function () {
   document.addEventListener('keydown', e => {
     if (e.key === 'Escape') {
       modal.classList.remove('is-open');
+      service_modal.classList.remove('is-open');
     }
   });
 });
