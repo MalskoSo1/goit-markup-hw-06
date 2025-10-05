809eb95ae2cac1dc4dd8a24d5e11cb731e68971a
Ліза Гоцко <filani9818@gmail.com>
2025-10-05 11:33:30 +0300

    some changes

--- js/script.js
+++ js/script.js
@@ -6,33 +6,33 @@ document.addEventListener('DOMContentLoaded', function () {
   const close = document.querySelector('.close');
   const service_close = document.querySelector('.modal-close-btn');
   const content = document.querySelector('.content');
+  const body = document.querySelector('.main-screen');
 
   button.addEventListener('click', () => {
     modal.classList.add('is-open');
+    body.classList.add('modal-open');
   });
 
   service_button.addEventListener('click', () => {
     service_modal.classList.add('is-open');
+    body.classList.add('modal-open');
   });
 
   close.addEventListener('click', () => {
     modal.classList.remove('is-open');
+    body.classList.remove('modal-open');
   });
 
   service_close.addEventListener('click', () => {
     service_modal.classList.remove('is-open');
-  });
-
-  modal.addEventListener('click', e => {
-    if (!content.contains(e.target)) {
-      modal.classList.remove('is-open');
-    }
+    body.classList.remove('modal-open');
   });
 
   document.addEventListener('keydown', e => {
     if (e.key === 'Escape') {
       modal.classList.remove('is-open');
       service_modal.classList.remove('is-open');
+      body.classList.remove('modal-open');
     }
   });
 });
